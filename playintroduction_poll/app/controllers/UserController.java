4a784a7ac518def86be9f7ba4397598ac53b18ef
irrigera <axel.irriger@msg-systems.com>
2013-02-13 20:19:02 +0100

    Small changes to ensure consistency

--- playintroduction_poll/app/controllers/UserController.java
+++ playintroduction_poll/app/controllers/UserController.java
@@ -12,102 +12,128 @@ import util.UserMongoBL;
 public class UserController extends Controller {
 
 	private static Form<RegisterLoginForm> registerForm = form(RegisterLoginForm.class);
-	public static RegisterLoginForm user = null;
-	
-	public static Result startUserRegister(){
-		if(Logger.isDebugEnabled()){
+
+	public static Result startUserRegister() {
+		if (Logger.isDebugEnabled()) {
 			Logger.debug("> UserController.startUserRegister()");
 		}
+
 		long start = System.currentTimeMillis();
 		Content html = views.html.userRegister.render(registerForm);
 		long end = System.currentTimeMillis();
-		if(Logger.isDebugEnabled()){
+		if (Logger.isDebugEnabled()) {
 			Logger.debug("Register page rendered in " + (end - start) + " ms");
 		}
-		return ok(views.html.pageframe.render("content",html));
+
+		final Result result = ok(views.html.pageframe.render("content", html));
+
+		if (Logger.isDebugEnabled()) {
+			Logger.debug("< UserController.startUserRegister()");
+		}
+		return result;
 	}
-	
-	public static Result registerUser(){
-		if(Logger.isDebugEnabled()){
+
+	public static Result registerUser() {
+		if (Logger.isDebugEnabled()) {
 			Logger.debug("> UserController.registerUser()");
 		}
-		user = registerForm.bindFromRequest().get();
-		if(user.username == null || user.username.equals("")||user.password == null||
-				user.password.equals("")||user.email==null||user.email.equals("")){
-			user = null; //try again
-			Content html = views.html.userRegister.render(registerForm);
-			return ok(views.html.pageframe.render("content",html));
-		}else{
-			UserMongoEntity userEntity = new UserMongoEntity();
+
+		final RegisterLoginForm user = registerForm.bindFromRequest().get();
+
+		Result result = null;
+
+		if (user.username == null || user.username.equals("")
+				|| user.password == null || user.password.equals("")
+				|| user.email == null || user.email.equals("")) {
+			final Content html = views.html.userRegister.render(registerForm);
+			result = ok(views.html.pageframe.render("content", html));
+		} else {
+			final UserMongoEntity userEntity = new UserMongoEntity();
 			userEntity.username = user.username;
+			// TODO Passwords should be hashed ;-)
 			userEntity.password = user.password;
 			userEntity.email = user.email;
-			
+
 			long start = System.currentTimeMillis();
 			UserMongoBL.saveUser(userEntity);
 			long end = System.currentTimeMillis();
-			if(Logger.isDebugEnabled()){
+			if (Logger.isDebugEnabled()) {
 				Logger.debug(" User saved in " + (end - start) + " ms");
+
+				if (Logger.isTraceEnabled()) {
+					Logger.trace("Username: " + user.username);
+					Logger.trace("Email: " + user.email);
+				}
 			}
-			if(Logger.isTraceEnabled()){
-				Logger.trace("Saved User:");
-				Logger.trace("Username: " + user.username);
-				Logger.trace("Email: " + user.email);
-			}
-			return redirect("/"); //redirect to main page
+
+			result = redirect("/"); // redirect to main page
 		}
+
+		if (Logger.isDebugEnabled()) {
+			Logger.debug("< UserController.registerUser()");
+		}
+		return result;
 	}
-	
-	public static Result startLogin(){
-		if(Logger.isDebugEnabled()){
+
+	public static Result startLogin() {
+		if (Logger.isDebugEnabled()) {
 			Logger.debug("> UserController.startLogin()");
 		}
+
 		long start = System.currentTimeMillis();
 		Content html = views.html.login.render(registerForm);
 		long end = System.currentTimeMillis();
-		if(Logger.isDebugEnabled()){
+
+		final Result result = ok(views.html.pageframe.render("content", html));
+		if (Logger.isDebugEnabled()) {
 			Logger.debug("Login page loaded in " + (end - start) + " ms");
+			Logger.debug("< UserController.startLogin()");
 		}
-		return ok(views.html.pageframe.render("content",html));
+		return result;
 	}
-	
-	public static Result login(){
-		if(Logger.isDebugEnabled()){
+
+	public static Result login() {
+		if (Logger.isDebugEnabled()) {
 			Logger.debug("> UserController.login()");
 		}
-		if(user == null){
-			user = registerForm.bindFromRequest().get();
-			if(user.username == null || user.username.equals("")||user.password == null||
-					user.password.equals("")){
-				user = null; //try again
-				if(Logger.isTraceEnabled()){
-					Logger.trace("Not all required fields are filled.");
-				}
-			}else{
-				UserMongoEntity userEntity = new UserMongoEntity(user);
-				long start = System.currentTimeMillis();
-				userEntity = UserMongoBL.loadUser(userEntity);
-				long end = System.currentTimeMillis();
-				if(Logger.isDebugEnabled()){
-					Logger.debug("User loaded in " + (end-start) + " ms");
-				}
-				if(userEntity != null){
-					user.username = userEntity.username;
-					user.password = userEntity.password;
-					user.email = userEntity.email;
-					if(Logger.isTraceEnabled()){
-						Logger.trace("Logged in User:");
-						Logger.trace("Username: " + user.username);
-						Logger.trace("Email: " + user.email);
-					}
-				}else{
-					user = null;
-				}
+
+		// TODO Not sure what you're doing in this method ... 
+		
+		Result result = null;
+		
+		RegisterLoginForm user = registerForm.bindFromRequest().get();
+		if (user.username == null || user.username.equals("")
+				|| user.password == null || user.password.equals("")) {
+			if (Logger.isTraceEnabled()) {
+				Logger.trace("Not all required fields are filled.");
 			}
-		}else{
-			user = null;
+			result = startLogin();
+		} else {
+			UserMongoEntity userEntity = new UserMongoEntity(user);
+
+			long start = System.currentTimeMillis();
+			userEntity = UserMongoBL.loadUser(userEntity);
+			long end = System.currentTimeMillis();
+			if (Logger.isDebugEnabled()) {
+				Logger.debug("User loaded in " + (end - start) + " ms");
+			}
+			
+			if (userEntity != null) {
+				user.username = userEntity.username;
+				user.password = userEntity.password;
+				user.email = userEntity.email;
+				if (Logger.isTraceEnabled()) {
+					Logger.trace("Logged in User:");
+					Logger.trace("Username: " + user.username);
+					Logger.trace("Email: " + user.email);
+				}
+			} else {
+				user = null;
+			}
 		}
+
 		Content html = views.html.login.render(registerForm);
-		return ok(views.html.pageframe.render("content",html));
+		result = ok(views.html.pageframe.render("content", html));
+		return result;
 	}
 }
