f0f05d632a11e6107f02c8f580489a8898801006
alexmsg <alexandruvladut@gmail.com>
2013-02-19 15:32:53 +0200

    Project LocalActor added to GitHub

---
+++ localActorApp/app/forms/MessageForm.java
@@ -0,0 +1,18 @@
+/*
+ * localActorApp
+ * MessageForm
+ * Created on 18.02.2013
+ */
+
+package forms;
+
+/**
+ * MessageForm.
+ * @author vladutb
+ * @version 1.0
+ * @since 18.02.2013
+ */
+public class MessageForm {
+    /** <code>message</code> */
+    public String message;
+}
